49e34fcda1a2e4aa39ed64c0eeb624016b933077
ahensh91 <ahensh91@gmail.com>
2021-03-19 19:59:32 -0500

    Infographic 1 add

---
+++ docs/assets/js/fancybox.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    /* Apply fancybox to multiple items */
+
+    $("a.iframe").fancybox({
+        'width': 640, // or whatever you want
+        'height': 480, // or whatever you want
+        'type': 'iframe'
+    });
+
+});
